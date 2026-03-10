d36f814f82ad664f510d5ead5803af817b5274d0
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-03-10 11:29:44 -0300

    GET Products by department name

--- src/main/java/com/devsuperior/workshopcassandra/services/ProductService.java
+++ src/main/java/com/devsuperior/workshopcassandra/services/ProductService.java
@@ -7,8 +7,10 @@ import com.devsuperior.workshopcassandra.services.exceptions.ResourceNotFoundExc
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
@@ -24,5 +26,10 @@ public class ProductService {
     private Product getById(UUID id) {
         Optional<Product> result = repository.findById(id);
         return result.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado"));
+    }
+
+    public List<ProductDTO> findByDepartment(String department) {
+        List<Product> list = repository.findByDepartment(department);
+        return list.stream().map(ProductDTO::new).collect(Collectors.toList());
     }
 }
